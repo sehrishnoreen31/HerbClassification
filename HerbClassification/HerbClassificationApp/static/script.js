88cc066b2f347ff81fec678526ba3f7c8796ec3a
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-06-14 07:20:18 +0500

    UI Updates

---
+++ HerbClassification/HerbClassificationApp/static/script.js
@@ -0,0 +1,8 @@
+let loadFile = function(event) {
+    let output = document.getElementById('preview');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) // free memory
+    }
+  };
+
